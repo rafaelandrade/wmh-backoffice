6ec2eafe676128743689de4cd1ae5e4361aff80c
rafaelandrade <rafasouza@protonmail.com>
2021-09-19 02:24:30 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # wmh-backoffice
+
+
+# Install Dependencies
+yarn install
+
+to execute run: yarn dev:server
---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.use(routes);
+
+
+app.listen(5003);
