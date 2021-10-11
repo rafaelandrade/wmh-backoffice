bda27529d6df254ef9c4cc5f20fd07c1fb1ae399
rafaelandrade <rafasouza@protonmail.com>
2021-10-11 14:50:40 -0300

    fix implementation

--- src/routes/v1/health/index.ts
+++ src/routes/v1/health/index.ts
@@ -1,4 +1,5 @@
 import Router from 'express-promise-router'
+import { Request, Response, NextFunction } from 'express'
 
 import { HealthController } from '../../../app/controllers/HealthController'
 import { requireHealthToken } from '../../../app/middleware'
@@ -7,6 +8,8 @@ const healthController = new HealthController()
 
 const router = Router()
 
-router.get('/', requireHealthToken, healthController.show)
+router.get('/', requireHealthToken, async (request: Request, response: Response, next: NextFunction) => {
+  return healthController.show(request, response, next)
+})
 
 export { router as healthRouter }
